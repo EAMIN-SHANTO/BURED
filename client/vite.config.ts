ce4ff987095ca959bef0a290ee331411a0b75a24
eamin <md.eamin519@gmail.com>
2025-02-05 03:41:05 +0600

    error fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './',
   publicDir: 'public',
   resolve: {
     alias: {
@@ -16,6 +16,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
+    copyPublicDir: true,
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name][extname]',
